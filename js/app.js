43c6795cf9e919787f1467d494c44434465a007e
yousufmiah <yousufweb99@gmail.com>
2022-03-11 00:20:33 +0600

    update image

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30
-    ? "text"
+    ? text
     : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -59,15 +59,16 @@ const createPost = (post) => {
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
-              <div class="post__header">
+
+
+
+                <div class="post__header">
                 <div class="post__profile">
-                  <a
-                    href="https://github.com/ProgrammingHero1"
-                    target="_blank"
-                    class="post__avatar"
-                  >
-                  
-                    <img src="${image}" alt="User Picture" />
+
+                <a class="post__avatar">
+                <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                
+                <img src="${image}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
